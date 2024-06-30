1cb75c2e28e3ee98f0e6dd5cec855f2cb747b3c2
Petr <petaschlup@seznam.cz>
2024-06-30 21:15:00 +0200

    New test testing execution times, was for caching, which I gave up

--- tests/video/frame_reader.py
+++ tests/video/frame_reader.py
@@ -5,7 +5,7 @@ import cv2
 import decord
 import numpy as np
 from unittest.mock import patch, MagicMock
-
+import time
 from detectflow.video.frame_reader import FrameReader
 
 class TestFrameReader(unittest.TestCase):
@@ -85,6 +85,55 @@ class TestFrameReader(unittest.TestCase):
 
 # TODO: Add test fro decord. Last time I had trouble mocking the frame for assertion at the end. It seemed to wokr though.
 
-if __name__ == '__main__':
+class TestFrameReader_realtest(unittest.TestCase):
+
+    def setUp(self):
+        self.video_path = r"D:\Dílna\Kutění\Python\DetectFlow\tests\video\resources\GR2_L1_TolUmb2_20220524_07_44.mp4"
+        # Ensure the test file exists
+        assert os.path.exists(self.video_path), f"Test video file does not exist: {self.video_path}"
+
+    def test_read_video_frame(self):
+        frame_reader = FrameReader(self.video_path)
+        frames = frame_reader.read_video_frame([0, 10, 20], stream=False)
+
+        self.assertIsInstance(frames, list)
+        self.assertEqual(len(frames), 3)
+
+        for frame_info in frames:
+            self.assertIn("frame_number", frame_info)
+            self.assertIn("frame", frame_info)
+            self.assertIsInstance(frame_info["frame_number"], int)
+            self.assertIsNotNone(frame_info["frame"])
+
+    def test_read_video_frame_with_caching(self):
+        import random
+        durations = []
+
+        def read_frames(f):
+            frame_nums = [random.randint(0, 100) for _ in range(10)]
+            start_time = time.time()
+            frame_reader = FrameReader(f)
+            frame_reader.read_video_frame(frame_nums, stream=False)
+            duration = time.time() - start_time
+            durations.append(duration)
+
+        files = [
+            r"D:\Dílna\Kutění\Python\DetectFlow\tests\video\resources\GR2_L1_TolUmb2_20220524_07_44.mp4",
+            r"D:\Dílna\Kutění\Python\DetectFlow\tests\video\resources\CZ2_T1_AciArv02_20210619_23_21.mp4",
+            r"D:\Dílna\Kutění\Python\DetectFlow\tests\video\resources\CZ2_T1_AciArv04_20210623_11_05.avi",
+            r"D:\Dílna\Kutění\Python\DetectFlow\tests\video\resources\GR2_L1_TolUmb2_20220524_07_44.mp4"
+        ]
+        for f in files:
+            read_frames(f)
+
+        print("Call durations:", durations)
+
+        # Check that the subsequent calls are faster
+        self.assertTrue(durations[1] < durations[0], "Second call should be faster due to caching.")
+        self.assertTrue(durations[2] < durations[0], "Third call should be faster due to caching.")
+
+
+if __name__ == "__main__":
     unittest.main()
+
 
