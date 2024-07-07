4f14bc9d8df2a53e1ed5933d52fa57f7314d1f88
Petr <petaschlup@seznam.cz>
2024-07-07 02:03:19 +0200

    Moved json load and save to separate file

--- detectflow/utils/__init__.py
+++ detectflow/utils/__init__.py
@@ -6,7 +6,7 @@ import json
 import re
 import platform
 import logging
-
+from detectflow.utils.file import json_load, json_save
 from detectflow.config import S3_CONFIG
 
 MACOS, LINUX, WINDOWS = (platform.system() == x for x in ['Darwin', 'Linux', 'Windows']) # operating system
@@ -60,64 +60,6 @@ def get_user_config_dir(sub_dir='detectflow'):
 
 USER_CONFIG_DIR = Path(os.getenv('DF_CONFIG_DIR') or get_user_config_dir())  # Detectflow settings dir
 SETTINGS_JSON = USER_CONFIG_DIR / 'settings.json'
-
-
-def json_save(file='data.json', data=None, header=''):
-    """
-    Save JSON data to a file.
-
-    Args:
-        file (str, optional): File name. Default is 'data.json'.
-        data (dict): Data to save in JSON format.
-        header (str, optional): JSON header to add (will be ignored in JSON).
-
-    Returns:
-        (None): Data is saved to the specified file.
-    """
-    if data is None:
-        data = {}
-    file = Path(file)
-    if not file.parent.exists():
-        # Create parent directories if they don't exist
-        file.parent.mkdir(parents=True, exist_ok=True)
-
-    # Convert Path objects to strings
-    valid_types = int, float, str, bool, list, tuple, dict, type(None)
-    for k, v in data.items():
-        if not isinstance(v, valid_types):
-            data[k] = str(v)
-
-    # Dump data to file in JSON format
-    with open(file, 'w', errors='ignore', encoding='utf-8') as f:
-        if header:
-            f.write(header)  # JSON doesn't support headers, but we write it as a comment
-        json.dump(data, f, ensure_ascii=False, indent=4)
-
-
-def json_load(file='data.json', append_filename=False):
-    """
-    Load JSON data from a file.
-
-    Args:
-        file (str, optional): File name. Default is 'data.json'.
-        append_filename (bool): Add the JSON filename to the JSON dictionary. Default is False.
-
-    Returns:
-        (dict): JSON data and file name.
-    """
-    assert Path(file).suffix == '.json', f'Attempting to load non-JSON file {file} with json_load()'
-    with open(file, errors='ignore', encoding='utf-8') as f:
-        s = f.read()  # string
-
-        # Remove special characters
-        if not s.isprintable():
-            s = re.sub(r'[^\x09\x0A\x0D\x20-\x7E\x85\xA0-\uD7FF\uE000-\uFFFD\U00010000-\U0010ffff]+', '', s)
-
-        # Add JSON filename to dict and return
-        data = json.loads(s) or {}  # always return a dict (json.loads() may return None for empty files)
-        if append_filename:
-            data['json_file'] = str(file)
-        return data
 
 
 class Settings(dict):
--- detectflow/utils/file.py
+++ detectflow/utils/file.py
@@ -30,6 +30,65 @@ def compare_file_sizes(file1, file2, tolerance=0.01):
     else:
         return False
 
+
+def json_save(file='data.json', data=None, header=''):
+    """
+    Save JSON data to a file.
+
+    Args:
+        file (str, optional): File name. Default is 'data.json'.
+        data (dict): Data to save in JSON format.
+        header (str, optional): JSON header to add (will be ignored in JSON).
+
+    Returns:
+        (None): Data is saved to the specified file.
+    """
+    if data is None:
+        data = {}
+    file = Path(file)
+    if not file.parent.exists():
+        # Create parent directories if they don't exist
+        file.parent.mkdir(parents=True, exist_ok=True)
+
+    # Convert Path objects to strings
+    valid_types = int, float, str, bool, list, tuple, dict, type(None)
+    for k, v in data.items():
+        if not isinstance(v, valid_types):
+            data[k] = str(v)
+
+    # Dump data to file in JSON format
+    with open(file, 'w', errors='ignore', encoding='utf-8') as f:
+        if header:
+            f.write(header)  # JSON doesn't support headers, but we write it as a comment
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+def json_load(file='data.json', append_filename=False):
+    """
+    Load JSON data from a file.
+
+    Args:
+        file (str, optional): File name. Default is 'data.json'.
+        append_filename (bool): Add the JSON filename to the JSON dictionary. Default is False.
+
+    Returns:
+        (dict): JSON data and file name.
+    """
+    assert Path(file).suffix == '.json', f'Attempting to load non-JSON file {file} with json_load()'
+    with open(file, errors='ignore', encoding='utf-8') as f:
+        s = f.read()  # string
+
+        # Remove special characters
+        if not s.isprintable():
+            s = re.sub(r'[^\x09\x0A\x0D\x20-\x7E\x85\xA0-\uD7FF\uE000-\uFFFD\U00010000-\U0010ffff]+', '', s)
+
+        # Add JSON filename to dict and return
+        data = json.loads(s) or {}  # always return a dict (json.loads() may return None for empty files)
+        if append_filename:
+            data['json_file'] = str(file)
+        return data
+
+
 def yolo_label_save(txt_file: str, boxes: np.ndarray):
     """
     Save YOLO formatted numpy array to txt file.
