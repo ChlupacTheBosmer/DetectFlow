e3dae38be10f37a40f74b8face4f5ab60d8817ff
Petr <petaschlup@seznam.cz>
2024-07-07 02:26:45 +0200

    Overhaul, fixed recursion depth

--- detectflow/handlers/ssh_handler.py
+++ detectflow/handlers/ssh_handler.py
@@ -2,16 +2,23 @@ import subprocess
 import os
 from typing import Optional
 import paramiko
+import socket
 
 
 class SSHHandler:
-    def __init__(self):
+    def __init__(self, username, password, remote_host, key_path='/.ssh/id_rsa'):
 
-        self.ssh_key_path = None
+        self.ssh_key_path = key_path
+        self.username = username
+        self.password = os.getenv('SSH_PASSWORD') if not password else password
+        self.remote_host = remote_host
+
+        self.ssh_client = self.connect(self.username, self.password, self.remote_host)
+        self.sftp_client = self.open_sftp(self.ssh_client)
 
     def generate_key(self, ssh_key_path: Optional[str] = None, regenerate: bool = True):
 
-        self.ssh_key_path = ssh_key_path if ssh_key_path is not None else '/.ssh/id_rsa'
+        self.ssh_key_path = ssh_key_path if ssh_key_path is not None else self.ssh_key_path
 
         os.makedirs(os.path.dirname(self.ssh_key_path), exist_ok=True)
 
@@ -70,13 +77,39 @@ class SSHHandler:
             return False
 
     @staticmethod
-    def authenticate(username, password, remote_host, key_path):
+    def authenticate(username, password, remote_host, key_path='/.ssh/id_rsa'):
 
         # Prepare SSH keys
-        ssh_handler = SSHHandler()
+        ssh_handler = SSHHandler(username, password, remote_host, key_path)
         key_path = ssh_handler.generate_key(key_path)
         if key_path is None:
             raise RuntimeError("SSH key generation failed. Authentication process terminated.")
         if not ssh_handler.upload_key(key_path, username, remote_host, password):
             print("SSH key upload failed. Key might already be uploaded. Authentication might not work.")
+
+    @staticmethod
+    def connect(username, password, remote_host):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            ssh.connect(remote_host, username=username, password=password)
+        except socket.gaierror as e:
+            print(f"Failed to resolve hostname {remote_host}. Please check the hostname and try again.")
+            return None
+        except Exception as e:
+            print(f"An error occurred while trying to connect: {e}")
+            return None
+        return ssh
+
+    @staticmethod
+    def open_sftp(ssh):
+        return ssh.open_sftp()
+
+    def disconnect(self):
+        if self.sftp_client:
+            self.sftp_client.close()
+        if self.ssh_client:
+            self.ssh_client.close()
+
+
 
