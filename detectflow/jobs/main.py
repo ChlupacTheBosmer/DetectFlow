5f0e7ef4e6d615889406dd4e1a6d955b534c908f
Petr <petaschlup@seznam.cz>
2024-07-07 02:50:28 +0200

    Main PBS job script

---
+++ detectflow/jobs/main.py
@@ -0,0 +1,137 @@
+import argparse
+import logging
+import os
+import sys
+import json
+from typing import Optional, Dict, Any
+from setup_env import setup_environment
+
+#setup_environment()
+
+# Now import detectflow package
+from detectflow.utils.config import load_json_config, load_ini_config, load_config, merge_configs
+from detectflow.process.database_manager import start_db_manager
+from detectflow.config import S3_CONFIG, DETECTFLOW_DIR
+from detectflow.callbacks.orchestrator_process_video import process_video_callback
+from detectflow.manipulators.dataloader import Dataloader
+from detectflow.process.orchestrator import Orchestrator  # Assuming the Orchestrator class is in orchestrator.py
+
+
+def setup_logging(log_file: Optional[str] = None):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+    stream_handler.propagate = False
+    logger.addHandler(stream_handler)
+
+    if log_file:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+
+def main(config_path: str, config_format: str, log_file: Optional[str], **kwargs):
+    if kwargs.get('verbose') == 'false':
+        os.environ['YOLO_VERBOSE'] = 'false'
+
+    if not kwargs.get('log_file', None):
+        log_file = os.path.join(kwargs.get('scratch'), 'orchestrator.log')
+
+    #setup_logging(log_file)
+
+    file_config = load_config(config_path, config_format)
+    merged_config = merge_configs(file_config, kwargs)
+
+    # Sort Kwargs
+    orchestrator_kwargs_keys = {
+        'batch_size',
+        'max_workers',
+        'force_restart',
+        'input_data',
+        'checkpoint_dir',
+        'task_name',
+        'scratch_path',
+        'user_name',
+        'dataloader',
+        'process_task_callback'
+    }
+
+    database_manager_kwargs_keys = {
+        'db_batch_size',
+        'db_backup_interval'
+    }
+
+    orchestrator_kwargs = {key: value for key, value in kwargs.items() if key in orchestrator_kwargs_keys}
+    database_manager_kwargs = {key.replace('db_', ''): value for key, value in merged_config.items() if key in database_manager_kwargs_keys}
+    other_kwargs = {key: value for key, value in merged_config.items() if key not in orchestrator_kwargs_keys}
+
+    logging.info("Initializing Database Manager...")
+    try:
+        db_man_info = start_db_manager(**database_manager_kwargs)
+    except Exception as e:
+        logging.error(f"An error occurred when initializing Database Manager: {e}")
+        sys.exit(1)
+
+    logging.info("Initializing Dataloader...")
+    try:
+        dataloader = Dataloader(S3_CONFIG)
+    except Exception as e:
+        logging.error(f"An error occurred when initializing Dataloader: {e}")
+        sys.exit(1)
+
+    logging.info("Packing Orchestrator Config...")
+    try:
+        callback_kwargs = {
+            "db_manager_control_queue": db_man_info.get("control_queue", None),
+            "db_manager_data_queue": db_man_info.get("data_queue", None),
+            **other_kwargs
+        }
+    except Exception as e:
+        logging.error(f"An error occurred when packing Orchestrator Config: {e}")
+        sys.exit(1)
+
+    logging.info("Initializing Orchestrator...")
+    try:
+        orchestrator = Orchestrator(config_path=config_path, config_format=config_format, dataloader=dataloader, parallelism="process", **orchestrator_kwargs, **callback_kwargs)
+        orchestrator.run()
+        logging.info("Orchestrator run completed successfully.")
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run Orchestrator with specified configuration.")
+    parser.add_argument('--config_path', type=str, help="Path to the configuration file.")
+    parser.add_argument('--config_format', type=str, choices=['json', 'ini'], default='json', help="Configuration file format.")
+    parser.add_argument('--log_file', type=str, help="Path to the log file.")
+
+    # Additional keyword arguments
+    parser.add_argument('--batch_size', type=int, help="Batch size for processing.")
+    parser.add_argument('--max_workers', type=int, help="Maximum number of workers.")
+    parser.add_argument('--force_restart', help="Force restart the processing.")
+    parser.add_argument('--input_data', type=str, help="Input data path or identifier.")
+    parser.add_argument('--checkpoint_dir', type=str, help="Directory for checkpoints.")
+    parser.add_argument('--task_name', type=str, help="Name of the task.")
+    parser.add_argument('--scratch_path', type=str, help="Scratch path for temporary files.")
+    parser.add_argument('--user_name', type=str, help="User name.")
+    parser.add_argument('--process_task_callback', type=str, help="Callback function for processing tasks.")
+
+    # Database Manager arguments
+    parser.add_argument('--db_batch_size', type=int, help="Size of batch of data to process.")
+    parser.add_argument('--db_backup_interval', type=int, help="Interval for backing up the database.")
+
+    parser.add_argument('--verbose', type=str, default='false', help="Prediction progress verbose settings. Logging unaffected.")
+
+    args = parser.parse_args()
+
+    kwargs = {key: value for key, value in vars(args).items() if value is not None}
+
+    print("Arguments (args):", args)
+    print("Keyword Arguments (kwargs):", kwargs)
+
+    main(**kwargs)
+
