781eebb144ab47a2086930dfadff72f545658109
Petr <petaschlup@seznam.cz>
2024-07-23 00:31:43 +0200

    New model pointer

--- detectflow/models/__init__.py
+++ detectflow/models/__init__.py
@@ -3,6 +3,8 @@ import os
 
 VISITORS_MODEL = os.path.join(DETECTFLOW_DIR, 'models', 'visitors.pt')
 FLOWERS_MODEL = os.path.join(DETECTFLOW_DIR, 'models', 'flowers.pt')
+FLOWERS_MODEL_yolov10x = os.path.join(DETECTFLOW_DIR, 'models', 'flowers_yolov10x.pt')
+FLOWERS_MODEL_yolov9e = os.path.join(DETECTFLOW_DIR, 'models', 'flowers_yolov9e.pt')
 
 DEFAULT_MODEL_CONFIG = {
     0: {'path': FLOWERS_MODEL,
