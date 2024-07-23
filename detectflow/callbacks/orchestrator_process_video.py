a314624f5264c6c6da58694bf8faf151a8d7cbcc
Petr <petaschlup@seznam.cz>
2024-07-23 16:32:02 +0200

    Train data are backed up to specified location after every video.

--- detectflow/callbacks/orchestrator_process_video.py
+++ detectflow/callbacks/orchestrator_process_video.py
@@ -91,6 +91,7 @@ def process_video_callback(task: Task,
                            orchestrator_control_queue: Any,
                            **kwargs):
     CONFIG_MAP = {"scratch_path": str,
+                  "train_data_folder": str,
                   "db_manager_control_queue": object,
                   "db_manager_data_queue": object,
                   "resource_monitor_queue": object,
@@ -134,6 +135,7 @@ def process_video_callback(task: Task,
 
     # Unpack kwargs
     scratch = kwargs.get('scratch_path', None)
+    train_data_folder = kwargs.get('train_data_folder', None)
     db_manager_control_queue = kwargs.get('db_manager_control_queue', None)
     db_manager_data_queue = kwargs.get('db_manager_data_queue', None)
     resource_monitor_queue = kwargs.get('resource_monitor_queue', None)
@@ -339,6 +341,29 @@ def process_video_callback(task: Task,
             except Exception as e:
                 logging.error(f"{name} - Error when backing up database: {e}")
 
+        # Move the folder with train data to a specified location
+        try:
+            if not train_data_folder:
+                raise ValueError(f"{name} - Train data folder not defined. Skipped train data backup.")
+
+            if not recording_id or not video_id:
+                raise ValueError(f"{name} - Recording ID or video ID not defined. Skipped train data backup.")
+
+            if not dataloader:
+                raise ValueError(f"{name} - Dataloader not defined. Skipped train data backup.")
+
+            recording_train_folder = os.path.join(train_data_folder, recording_id)
+            os.makedirs(recording_train_folder, exist_ok=True)
+            train_folder = dataloader.locate_folder_local(r"train", scratch, selection_criteria='name')
+            if train_folder:
+                video_train_folder = str(os.path.join(train_folder, video_id))
+                destination_folder = os.path.join(recording_train_folder, video_id)
+                if os.path.isdir(video_train_folder):
+                    dataloader.move_folder(video_train_folder, destination_folder, overwrite=True, copy=False)
+                    logging.info(f"{name} - Moved train data folder: {video_train_folder} to {destination_folder}")
+        except Exception as e:
+            logging.error(f"{name} - Error when moving train data folder: {e}")
+
     # After processing, delete the files
     try:
         for video_path in valid + invalid:
@@ -346,10 +371,9 @@ def process_video_callback(task: Task,
                 dataloader.delete_file(video_path)
             else:
                 os.remove(video_path)
+        logging.info(f"{name} - Deleted video files.")
     except Exception as e:
-        logging.error(f"{task.name if hasattr(task, 'name') else 'Unknown task'} - Error when deleting files: {e}")
-    finally:
-        del dataloader
+        logging.error(f"{name} - Error when deleting files: {e}")
 
 
 def extract_worker_number(worker_name):
