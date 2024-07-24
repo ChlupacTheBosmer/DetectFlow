624489fe021e14df9768ff23411fd6d82117f93f
Petr <petaschlup@seznam.cz>
2024-07-24 10:17:02 +0200

    new model file.

--- detectflow/models/__init__.py
+++ detectflow/models/__init__.py
@@ -3,11 +3,11 @@ import os
 
 VISITORS_MODEL = os.path.join(DETECTFLOW_DIR, 'models', 'visitors.pt')
 FLOWERS_MODEL = os.path.join(DETECTFLOW_DIR, 'models', 'flowers.pt')
+FLOWERS_MODEL_yolov8n = os.path.join(DETECTFLOW_DIR, 'models', 'flowers_yolov8n.pt')
 FLOWERS_MODEL_yolov10x = os.path.join(DETECTFLOW_DIR, 'models', 'flowers_yolov10x.pt')
-FLOWERS_MODEL_yolov9e = os.path.join(DETECTFLOW_DIR, 'models', 'flowers_yolov9e.pt')
 
 DEFAULT_MODEL_CONFIG = {
-    0: {'path': FLOWERS_MODEL,
+    0: {'path': FLOWERS_MODEL_yolov8n,
         'conf': 0.1},
     1: {'path': VISITORS_MODEL,
         'conf': 0.1},
