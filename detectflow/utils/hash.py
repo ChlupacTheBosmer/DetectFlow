767e4c69558446956adb61236fdf143cb63bb266
Petr <petaschlup@seznam.cz>
2024-07-30 00:47:09 +0200

    New functions

--- detectflow/utils/hash.py
+++ detectflow/utils/hash.py
@@ -1,6 +1,13 @@
 import time
+import hashlib
 
-def get_numeric_hash():
+
+def get_timestamp_hash():
     # Get the current time in milliseconds since the epoch
     timestamp = int(time.time() * 1000)
     return timestamp
+
+
+def get_filepath_hash(file_path):
+    unique_hash = hashlib.md5((file_path + str(time.time())).encode()).hexdigest()
+    return unique_hash
