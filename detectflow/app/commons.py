c6fefa171630b164d54a9f2d0e03d8e1a123a553
Petr <petaschlup@seznam.cz>
2024-07-30 00:52:02 +0200

    Removed cyan, reserved for flowers

--- detectflow/app/commons.py
+++ detectflow/app/commons.py
@@ -5,7 +5,6 @@ COLORS = [
         QColor(0, 255, 0, 200),    # Green
         QColor(0, 0, 255, 200),    # Blue
         QColor(255, 0, 255, 200),  # Magenta
-        QColor(0, 255, 255, 200),  # Cyan
         QColor(255, 165, 0, 200),  # Orange
         QColor(75, 0, 130, 200),   # Indigo
         QColor(128, 0, 128, 200),  # Purple
