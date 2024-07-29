ac7b0fc4bf524ebbbff86aff098d608f51d33dfb
Petr <petaschlup@seznam.cz>
2024-07-30 00:44:36 +0200

    Added new function

--- detectflow/utils/cfg.py
+++ detectflow/utils/cfg.py
@@ -1,5 +1,7 @@
 import os
 import configparser
+from detectflow.config import S3_CONFIG
+from detectflow.utils import install_s3_config
 
 
 def parse_s3_config(cfg_file):
@@ -26,3 +28,27 @@ def parse_s3_config(cfg_file):
         return endpoint_url, access_key, secret_key
     else:
         return None, None, None
+
+
+def is_s3_config_valid(cfg_file):
+    if not os.path.exists(cfg_file):
+        return False
+
+    try:
+        s3_config = parse_s3_config(cfg_file)
+        if any(value is None for value in s3_config):
+            return False
+        else:
+            return True
+    except Exception as e:
+        return False
+
+
+def resolve_s3_config(cfg_dict: dict, cfg_file: str = S3_CONFIG):
+    if not is_s3_config_valid(cfg_file):
+        install_s3_config(cfg_dict.get('host_base'),
+                          cfg_dict.get('use_https', True),
+                          cfg_dict.get('access_key'),
+                          cfg_dict.get('secret_key'),
+                          cfg_dict.get('host_bucket'))
+
