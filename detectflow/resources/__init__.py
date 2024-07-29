fb5aab004616f0f2acb0af90292c74471cabf230
Petr <petaschlup@seznam.cz>
2024-07-30 00:51:40 +0200

    Added ALT ICONS

--- detectflow/resources/__init__.py
+++ detectflow/resources/__init__.py
@@ -2,12 +2,19 @@ import os
 
 RESOURCES_DIR = os.path.dirname(os.path.abspath(__file__))
 IMG_DIR = os.path.join(RESOURCES_DIR, "img")
+ALT_DIR = os.path.join(RESOURCES_DIR, "alt")
 
 # Create a dictionary ith all icon paths
 ICONS = {}
 for file in os.listdir(IMG_DIR):
     if file.endswith(".svg"):
         ICONS[file.split(".")[0]] = os.path.join(IMG_DIR, file)
+
+# Create a dictionary with all alternative image paths
+ALT_ICONS = {}
+for file in os.listdir(ALT_DIR):
+    if file.endswith(".svg"):
+        ALT_ICONS[file.split(".")[0]] = os.path.join(ALT_DIR, file)
 
 # Create a dictionary with all image paths
 IMGS = {}
